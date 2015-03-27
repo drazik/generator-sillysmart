250f1d026944c8eadf93a67c20b4285aaa3a9836
Cyrille Perois <cyrille.perois@wandi.fr>
2015-03-27 12:07:29 +0100

    close #8

--- generators/gulp/index.js
+++ generators/gulp/index.js
@@ -3,12 +3,17 @@
 var yeoman = require('yeoman-generator');
 
 module.exports = yeoman.generators.Base.extend({
-    initialize: function () {
+    constructor: function() {
+        yeoman.generators.Base.apply(this, arguments);
+
+        this.argument('packageName', { type: String, optional: true, defaults: 'your-package-name' });
+
+        this.packageName = this.packageName.toLowerCase().split(' ').join('-');
         this.dependencies = ['require-dir', 'gulp', 'gulp-plumber', 'gulp-util', 'minimist'];
     },
     copyFiles: function() {
         this.copy('gulpfile.js', 'gulpfile.js');
-        this.template('package.json', 'package.json', { packageName: 'fake-package-name' });
+        this.template('package.json', 'package.json', { packageName: this.packageName });
     },
     installDependencies: function() {
         this.npmInstall(this.dependencies, { saveDev: true });
